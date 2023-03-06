3c3fcc37179a0111866175d0c8620e600289eca7
fbaun <fbaun@HIGHTOWER>
2023-03-06 21:25:39 +0100

    -

--- HurtigTest/Cykel.cs
+++ HurtigTest/Cykel.cs
@@ -8,24 +8,36 @@ namespace HurtigTest
 {
     public class Cykel
     {
-        
-
         public int StelNr { get; set; }
         public string Model { get; set; }
         public int År { get; set; }
 
-        public Cykel(int stelNr, string model, int år)
+        public int Pris { get; set; }
+
+        public Cykel(int stelNr, string model, int år, int pris)
         {
             StelNr = stelNr;
             Model = model;
             År = år;
-            
+            Pris = pris;
         }
 
 
         public override string ToString()
         {
             return $"StelNr: {StelNr}, Model: {Model}, År: {År}";
+        }
+
+        public static int EvertonRabatPris(List<Cykel> list) {
+            int discount = 0;
+            list.ForEach(x =>
+            {
+                if (x.År < 2020 && x.Model.Equals("Everton"))
+                {
+                    discount = x.Pris - 250;
+                }
+            });
+            return discount;
         }
 
     }
--- HurtigTest/Program.cs
+++ HurtigTest/Program.cs
@@ -1,13 +1,14 @@
 ﻿
 using HurtigTest;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
 
 
 //Opgave 1
-Cykel c1 = new Cykel(12345, "Everton", 2013);
-Cykel c2 = new Cykel(67890, "Mountain bike", 2010);
-Cykel c3 = new Cykel(11111, "Racer cykel", 2023);
-Cykel c4 = new Cykel(10001, "Everton", 2021);
+Cykel c1 = new Cykel(12345, "Everton", 2013, 1000);
+Cykel c2 = new Cykel(67890, "Mountain bike", 2010, 1000);
+Cykel c3 = new Cykel(11111, "Racer cykel", 2023, 1000);
+Cykel c4 = new Cykel(10001, "Everton", 2021, 1000);
 
 //Opgave 2
 List<Cykel> cykelList = new List<Cykel>();
@@ -16,33 +17,75 @@ cykelList.Add(c2);
 cykelList.Add(c3);
 cykelList.Add(c4);
 
-//Opgave 3
-Console.WriteLine("\nOpgave 3");
-foreach (Cykel c in cykelList) {
-    if (c.År > 2020) {
-        Console.WriteLine(c.ToString());
-    }
-}
+////Opgave 3
+//Console.WriteLine("\nOpgave 3");
+//foreach (Cykel c in cykelList) {
+//    if (c.År > 2020) {
+//        Console.WriteLine(c.ToString());
+//    }
+//}
 
 
-//Opgave 4
-Console.WriteLine("\nOpgave 4");
+////Opgave 4
+//Console.WriteLine("\nOpgave 4");
 
-Cykel lowestNr = cykelList.OrderBy(x => x.StelNr).First();
-Console.WriteLine(lowestNr.ToString());  
-
-
-//Opgave 5
-Console.WriteLine("\nOpgave 5");
-List<Cykel> cykelSorter = cykelList.OrderByDescending(x => x.StelNr).ToList();
-cykelSorter.ForEach(x => Console.WriteLine(x.ToString()));
+//Cykel lowestNr = cykelList.OrderBy(x => x.StelNr).First();
+//Console.WriteLine(lowestNr.ToString());  
 
 
-//Opgave 6
-Console.WriteLine("\nOpgave 6");
+////Opgave 5
+//Console.WriteLine("\nOpgave 5");
+//List<Cykel> cykelSorter = cykelList.OrderByDescending(x => x.StelNr).ToList();
+//cykelSorter.ForEach(x => Console.WriteLine(x.ToString()));
 
-List<Cykel> everton = cykelList.FindAll(x => x.Model.Equals("Everton"));
 
-everton.ForEach(x => Console.WriteLine(x.ToString()));
+////Opgave 6
+//Console.WriteLine("\nOpgave 6");
 
- 
+//List<Cykel> everton = cykelList.FindAll(x => x.Model.Equals("Everton"));
+
+//everton.ForEach(x => Console.WriteLine(x.ToString()));
+
+
+//Ekstra opgaver
+
+////Luca + Milo
+//Console.WriteLine("Input model:");
+//string model = Console.ReadLine();
+//List<Cykel> everton = cykelList.FindAll(x => x.Model.Equals(model));
+//everton.ForEach(x => Console.WriteLine(x.ToString()));
+
+//everton.ForEach(x => {
+//    if (x.År > 2018)
+//    {
+//        Console.WriteLine(x.ToString());
+//    }
+//});
+
+////Anders + Sarah
+//Console.WriteLine(Cykel.EvertonRabatPris(cykelList));
+
+//var sqlSyntax = from cykel in cykelList
+//                where cykel.Pris == 1000
+//                select cykel;
+
+//var fluentSyntax = cykelList.Where(cykel => cykel.Pris == 1000);
+
+//foreach (var cykel in sqlSyntax) {
+//    Console.WriteLine(cykel.ToString());
+//}
+
+//foreach (var cykel in fluentSyntax) {
+//    Console.WriteLine(cykel.ToString());
+//}
+
+//Daniel + Victor
+
+
+////Frederik + Julie
+//Random rnd = new Random();
+//int rndIndex = rnd.Next(cykelList.Count);
+//Console.WriteLine(cykelList[rndIndex]);
+
+
+
